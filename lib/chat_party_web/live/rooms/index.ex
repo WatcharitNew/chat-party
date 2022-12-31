3b3acf5227c6bff337a787ad2e2128e82ffa19b1
WatcharitNew <watcharit.new@gmail.com>
2022-12-31 20:53:33 +0700

    feat: add handle event get_messages

--- lib/chat_party_web/live/rooms/index.ex
+++ lib/chat_party_web/live/rooms/index.ex
@@ -2,7 +2,6 @@ defmodule ChatPartyWeb.RoomsLive.Index do
   use ChatPartyWeb, :live_view
 
   alias ChatParty.Room
-  alias ChatPartyWeb.RoomsLive
   alias ChatPartyWeb.RoomsView
 
   def render(assigns) do
@@ -12,16 +11,40 @@ defmodule ChatPartyWeb.RoomsLive.Index do
   def mount(_params, %{}, socket) do
     if connected?(socket), do: Process.send_after(self(), :update, 1000)
 
-    {:ok, assign(socket, :rooms, rooms())}
+    {
+      :ok,
+      assign(
+        socket,
+        %{
+          rooms: rooms(),
+          room: nil
+        }
+      )
+    }
   end
 
   def handle_info(:update, socket) do
     Process.send_after(self(), :update, 1000)
+    selected_room = if socket.assigns.room, do: room(socket.assigns.room.id), else: nil
 
-    {:noreply, assign(socket, :rooms, rooms())}
+    {
+      :noreply,
+      assign(socket, %{
+        rooms: rooms(),
+        room: selected_room
+      })
+    }
   end
 
-  defp rooms do
+  def handle_event("get_messages", %{"id" => id}, socket) do
+    {:noreply, assign(socket, %{room: room(id)})}
+  end
+
+  defp rooms() do
     Room.list_rooms()
+  end
+
+  defp room(id) do
+    Room.get_room!(id)
   end
 end
